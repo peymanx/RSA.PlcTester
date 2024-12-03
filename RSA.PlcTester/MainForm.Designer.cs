ac867ac1bbddb0070c8675d9e90479570227e06d
Peymanx <peyman.majidi@outlook.com>
2024-12-03 10:34:39 +0330

    Add project files.

---
+++ RSA.PlcTester/MainForm.Designer.cs
@@ -0,0 +1,350 @@
+﻿
+namespace RSA.PlcTester
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtIpAddress = new System.Windows.Forms.TextBox();
+            this.txtRack = new System.Windows.Forms.TextBox();
+            this.txtSlot = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblResult = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtReadValue = new System.Windows.Forms.TextBox();
+            this.txtWriteValue = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtReadAddress = new System.Windows.Forms.TextBox();
+            this.txtWriteAddress = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.Location = new System.Drawing.Point(88, 23);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(133, 20);
+            this.txtIpAddress.TabIndex = 0;
+            // 
+            // txtRack
+            // 
+            this.txtRack.Location = new System.Drawing.Point(88, 49);
+            this.txtRack.Name = "txtRack";
+            this.txtRack.Size = new System.Drawing.Size(133, 20);
+            this.txtRack.TabIndex = 1;
+            // 
+            // txtSlot
+            // 
+            this.txtSlot.Location = new System.Drawing.Point(88, 75);
+            this.txtSlot.Name = "txtSlot";
+            this.txtSlot.Size = new System.Drawing.Size(133, 20);
+            this.txtSlot.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblResult,
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 508);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(823, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblResult
+            // 
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(39, 17);
+            this.lblResult.Text = "Result";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP Address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Rack:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Slot:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(586, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(227, 340);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 44);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(143, 91);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(41, 296);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(239, 39);
+            this.ConnectButton.TabIndex = 9;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 39);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Write";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.writeButton_clicked);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 39);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Read";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.readButton_Clicked);
+            // 
+            // txtReadValue
+            // 
+            this.txtReadValue.Location = new System.Drawing.Point(56, 31);
+            this.txtReadValue.Name = "txtReadValue";
+            this.txtReadValue.Size = new System.Drawing.Size(133, 20);
+            this.txtReadValue.TabIndex = 13;
+            // 
+            // txtWriteValue
+            // 
+            this.txtWriteValue.Location = new System.Drawing.Point(56, 57);
+            this.txtWriteValue.Name = "txtWriteValue";
+            this.txtWriteValue.Size = new System.Drawing.Size(133, 20);
+            this.txtWriteValue.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Read:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Write:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Write:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 34);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Read:";
+            // 
+            // txtReadAddress
+            // 
+            this.txtReadAddress.Location = new System.Drawing.Point(55, 30);
+            this.txtReadAddress.Name = "txtReadAddress";
+            this.txtReadAddress.Size = new System.Drawing.Size(133, 20);
+            this.txtReadAddress.TabIndex = 17;
+            // 
+            // txtWriteAddress
+            // 
+            this.txtWriteAddress.Location = new System.Drawing.Point(55, 56);
+            this.txtWriteAddress.Name = "txtWriteAddress";
+            this.txtWriteAddress.Size = new System.Drawing.Size(133, 20);
+            this.txtWriteAddress.TabIndex = 16;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtReadAddress);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtWriteAddress);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Location = new System.Drawing.Point(342, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Address";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtWriteValue);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtReadValue);
+            this.groupBox2.Location = new System.Drawing.Point(342, 141);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 21;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Value";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.txtIpAddress);
+            this.groupBox3.Controls.Add(this.txtRack);
+            this.groupBox3.Controls.Add(this.txtSlot);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(41, 177);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(239, 114);
+            this.groupBox3.TabIndex = 22;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Config";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 530);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "MainForm";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIpAddress;
+        private System.Windows.Forms.TextBox txtRack;
+        private System.Windows.Forms.TextBox txtSlot;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtReadValue;
+        private System.Windows.Forms.TextBox txtWriteValue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtReadAddress;
+        private System.Windows.Forms.TextBox txtWriteAddress;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ RSA.PlcTester/MainForm.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using S7.Net; // استفاده از S7NetPlus
+using System.IO;
+using Newtonsoft.Json;
+
+namespace RSA.PlcTester
+{
+
+    public partial class MainForm : Form
+    {
+        private Plc plc;
+        private PlcSettings plcSettings;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadSettings();
+        }
+        private void LoadSettings()
+        {
+            try
+            {
+                // مسیر فایل JSON
+                string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "setting.json");
+
+                // خواندن و تبدیل JSON به شیء
+                string jsonContent = File.ReadAllText(jsonPath);
+                plcSettings = JsonConvert.DeserializeObject<PlcSettings>(jsonContent);
+
+                // نمایش تنظیمات در فرم (اختیاری)
+                txtIpAddress.Text = plcSettings.Plc.IpAddress;
+                txtRack.Text = plcSettings.Plc.Rack.ToString();
+                txtSlot.Text = plcSettings.Plc.Slot.ToString();
+                txtReadAddress.Text = plcSettings.Plc.ReadAddress;
+                txtWriteAddress.Text = plcSettings.Plc.WriteAddress;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"خطا در بارگذاری تنظیمات: {ex.Message}");
+            }
+        }
+
+
+
+
+        private void ConnectButton_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                // اتصال به PLC با تنظیمات خوانده‌شده
+                plc = new Plc(CpuType.S71200, plcSettings.Plc.IpAddress, (short)plcSettings.Plc.Rack, (short)plcSettings.Plc.Slot);
+                plc.Open();
+
+                lblStatus.Text = plc.IsConnected ? "متصل شد!" : "اتصال برقرار نشد.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"خطا در اتصال: {ex.Message}");
+            }
+        }
+
+        private void writeButton_clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (plc != null && plc.IsConnected)
+                {
+                    int newValue = Convert.ToInt32(txtWriteValue.Text);
+                    plc.Write(plcSettings.Plc.WriteAddress, newValue);
+                }
+                else
+                {
+                    MessageBox.Show("ابتدا به PLC متصل شوید.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"خطا در نوشتن داده‌ها: {ex.Message}");
+            }
+        }
+
+        private void readButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (plc != null && plc.IsConnected)
+                {
+                    object value = plc.Read(plcSettings.Plc.ReadAddress);
+                    txtReadValue.Text = value.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("ابتدا به PLC متصل شوید.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"خطا در خواندن داده‌ها: {ex.Message}");
+            }
+        }
+    }
+
+
+    // کلاس تنظیمات
+    public class PlcSettings
+    {
+        public PlcInfo Plc { get; set; }
+    }
+
+    public class PlcInfo
+    {
+        public string IpAddress { get; set; }
+        public int Rack { get; set; }
+        public int Slot { get; set; }
+        public string ReadAddress { get; set; }
+        public string WriteAddress { get; set; }
+    }
+}
